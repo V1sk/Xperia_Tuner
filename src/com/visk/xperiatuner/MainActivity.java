9be35a838d8c69b7ae11b47c42b85d2bcfb470e3
CJW <938548157@qq.com>
2014-05-10 11:55:03 +0800

    首次提交 - Xperia Tuner框架模板

---
+++ src/com/visk/xperiatuner/MainActivity.java
@@ -0,0 +1,230 @@
+package com.visk.xperiatuner;
+
+import com.visk.xperiatuner.cpucontrol.CpuControlFragment;
+import com.visk.xperiatunertest.R;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity implements
+OnPageChangeListener {
+
+	private final Handler handler = new Handler();
+
+	private PagerSlidingTabStrip tabs;
+	private ViewPager pager;
+	private MyPagerAdapter adapter;
+
+	private Drawable oldBackground = null;
+	private int currentColor = 0xFF666666;
+	private String[] flatColors = {"#555555", "#16a085", "#D26900", "#388E8E", "#34495e"};
+
+	private Drawable.Callback drawableCallback = new Drawable.Callback() {
+
+		@Override
+		public void invalidateDrawable(Drawable who) {
+			getActionBar().setBackgroundDrawable(who);
+		}
+
+		@Override
+		public void scheduleDrawable(Drawable who, Runnable what, long when) {
+			handler.postAtTime(what, when);
+		}
+
+		@Override
+		public void unscheduleDrawable(Drawable who, Runnable what) {
+			handler.removeCallbacks(what);
+		}
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+		pager = (ViewPager) findViewById(R.id.pager);
+		adapter = new MyPagerAdapter(getSupportFragmentManager());
+
+		pager.setAdapter(adapter);
+
+		final int pageMargin = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+						.getDisplayMetrics());
+		pager.setPageMargin(pageMargin);
+
+		tabs.setViewPager(pager);
+		tabs.setOnPageChangeListener(this);
+
+		changeColor(currentColor);
+	}
+
+	private void changeColor(int newColor) {
+
+		tabs.setIndicatorColor(newColor);
+
+		// change ActionBar color just if an ActionBar is available
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+			Drawable colorDrawable = new ColorDrawable(newColor);
+			Drawable bottomDrawable = getResources().getDrawable(
+					R.drawable.actionbar_bottom);
+			LayerDrawable ld = new LayerDrawable(new Drawable[] {
+					colorDrawable, bottomDrawable });
+
+			if (oldBackground == null) {
+
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					ld.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(ld);
+				}
+
+			} else {
+
+				TransitionDrawable td = new TransitionDrawable(new Drawable[] {
+						oldBackground, ld });
+
+				// workaround for broken ActionBarContainer drawable handling on
+				// pre-API 17 builds
+				// https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					td.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(td);
+				}
+
+				td.startTransition(200);
+
+			}
+
+			oldBackground = ld;
+
+			// http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+			getActionBar().setDisplayShowTitleEnabled(false);
+			getActionBar().setDisplayShowTitleEnabled(true);
+
+		}
+
+		currentColor = newColor;
+
+	}
+
+	/*public void onColorClicked(View v) {
+
+		int color = Color.parseColor(v.getTag().toString());
+		changeColor(color);
+
+	}*/
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("currentColor", currentColor);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		currentColor = savedInstanceState.getInt("currentColor");
+		changeColor(currentColor);
+	}
+
+	
+
+	public class MyPagerAdapter extends FragmentPagerAdapter {
+
+		private final String[] TITLES = { "CPU Control", "GPU Control", "I/O Tweaks",
+				"Memory Manager","Touch Screen" };
+
+		public MyPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return TITLES[position];
+		}
+
+		@Override
+		public int getCount() {
+			return TITLES.length;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			Fragment f = new Fragment();
+			switch (position) {
+			case 0:
+				f = new CpuControlFragment();
+				break;
+			case 1:
+				//f = new SuperAwesomeCardFragment();
+				break;
+			
+			default:
+				//changeColor(currentColor);
+				f = new CpuControlFragment();
+
+			}
+			
+			return f;
+		}
+
+	}
+
+
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+	}
+
+	@Override
+	public void onPageSelected(int pos) {
+		changeColor(Color.parseColor(flatColors[pos]));
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		
+		switch (item.getItemId()) {
+        case R.id.action_settings:
+            startActivity(new Intent(this, SettingsActivity.class));
+    }
+		
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+}
